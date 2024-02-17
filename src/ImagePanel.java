c2baa14971dcb0f5d20d57c6c2f64e5b2dd30300
blaqueee <kajrattobokelov2@gmail.com>
2024-02-17 16:03:42 +0600

    sample java desktop application

---
+++ src/ImagePanel.java
@@ -0,0 +1,25 @@
+import com.digitalpersona.uareu.Fid;
+import com.digitalpersona.uareu.Fid.Fiv;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class ImagePanel 
+	extends JPanel
+{
+	private static final long serialVersionUID = 5;
+	private BufferedImage m_image;
+	
+	public void showImage(Fid image){
+		Fiv view = image.getViews()[0];
+		m_image = new BufferedImage(view.getWidth(), view.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+		m_image.getRaster().setDataElements(0, 0, view.getWidth(), view.getHeight(), view.getImageData());
+		repaint();
+	} 
+	
+	public void paint(Graphics g) {
+		g.drawImage(m_image, 0, 0, null);
+	}
+
+}
